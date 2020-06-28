14bf9db789a19b7c09eb82964f17ed4c8c70463b
金城刚 <773900035@qq.com>
2020-06-28 10:38:47 +0800

    Merge branch 'master' of https://github.com/Ice-Cirno/HoshinoBot
    # Conflicts:
    #	.gitignore
    #	hoshino/config_example/__bot__.py
    #	hoshino/modules/groupmaster/chat.py
    #	hoshino/modules/groupmaster/group_notice.py
    #	hoshino/modules/pcrclanbattle/clanbattle/cmdv2.py
    #	hoshino/modules/priconne/arena/arena.py
    #	hoshino/modules/priconne/gacha/__init__.py
    #	hoshino/modules/priconne/gacha/config.json
    #	hoshino/modules/priconne/priconne_data.py
    #	hoshino/modules/priconne/query/query.py
    #	hoshino/util.py

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -1,85 +1,81 @@
 import random
-from datetime import timedelta
 
 from nonebot import on_command
-from hoshino import util
-from hoshino.res import R
-from hoshino.service import Service, Privilege as Priv
+
+from hoshino import R, Service, priv, util
+
 
 
 # basic function for debug, not included in Service('chat')
-@on_command('zai?', aliases=('在?', '在？', '在吗', '在么？', '在嘛', '在嘛？'))
+@on_command('zai?', aliases=('在?', '在？', '在吗', '在么？', '在嘛', '在嘛？'), only_to_me=True)
 async def say_hello(session):
     await session.send('はい！私はいつも貴方の側にいますよ！')
 
+sv = Service('chat', manage_priv=priv.SUPERUSER, visible=False)
 
-sv = Service('chat', manage_priv=Priv.SUPERUSER, visible=False)
-
-
-@sv.on_command('沙雕机器人', aliases=('沙雕機器人',), only_to_me=False)
-async def say_sorry(session):
-    await session.send('ごめんなさい！嘤嘤嘤(〒︿〒)')
+@sv.on_fullmatch(('沙雕机器人', '沙雕機器人'))
+async def say_sorry(bot, ev):
+    await bot.send(ev, 'ごめんなさい！嘤嘤嘤(〒︿〒)')
 
 
-@sv.on_command('老婆', aliases=('waifu', 'laopo'), only_to_me=True)
-async def chat_waifu(session):
-    if not sv.check_priv(session.ctx, Priv.SUPERUSER):
-        await session.send(R.img('laopo.jpg').cqcode)
+@sv.on_fullmatch(('老婆', 'waifu', 'laopo'), only_to_me=True)
+async def chat_waifu(bot, ev):
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, R.img('laopo.jpg').cqcode)
     else:
-        await session.send('mua~')
+        await bot.send(ev, 'mua~')
 
 
-@sv.on_command('老公', only_to_me=True)
-async def chat_laogong(session):
-    await session.send('你给我滚！', at_sender=True)
+@sv.on_fullmatch('老公', only_to_me=True)
+async def chat_laogong(bot, ev):
+    await bot.send(ev, '你给我滚！', at_sender=True)
+
 
 @sv.on_command('贴贴', aliases=('贴贴~', ), only_to_me=True)
-async def chat_tietie(session):
-    await session.send('贴贴~', at_sender=True)
-
-
-@sv.on_command('mua', only_to_me=True)
-async def chat_mua(session):
-    await session.send('笨蛋~', at_sender=True)
-
-
-@sv.on_command('来点星奏', only_to_me=False)
-async def seina(session):
-    await session.send(R.img('星奏.png').cqcode)
-
-
-@sv.on_command('我有个朋友说他好了', aliases=('我朋友说他好了',), only_to_me=False)
-async def ddhaole(session):
-    await session.send('那个朋友是不是你弟弟？')
-    # await util.silence(session.ctx, 30)
-
-
-@sv.on_command('我好了', only_to_me=False)
-async def nihaole(session):
-    await session.send('不许好，憋回去！')
-    # await util.silence(session.ctx, 30)
-
+async def chat_tietie(bot, ev):
+    await bot.send(ev, '贴贴~', at_sender=True)
 
 @sv.on_command('对不起', aliases=('优衣对不起', 'ue对不起'), only_to_me=True)
-async def chat_sorry(session):
+async def chat_sorry(bot, ev):
     duibuqi = ['你给我滚！', '带你骨灰扬了！',
                '\n第一次,有了喜欢的人\n还得到了一生的挚友\n两份喜悦相互重叠\n这双重的喜悦又带来了更多更多的喜悦\n本应已经得到了梦幻一般的幸福时光\n然而,为什么,会变成这样？', '明明是我先来的~']
-    await session.send(random.choice(duibuqi), at_sender=True)
+    await bot.send(ev, random.choice(duibuqi), at_sender=True)
 
 
 @sv.on_command('骂我', aliases=('老婆骂我', '找骂', '凌辱', '欺负', '继续', '大声点', '没吃饭么', '你再骂'))
-async def chat_sorry(session):
+async def chat_sorry(bot, ev):
     zuanyulu = ['wdnmd，闸总', '哦~', 'バカ', '変態', '无路赛', 'エロい犬', '八嘎hen tai无路赛', '我给你🐴一锤', '你🐴买菜必超级加倍', '你在想peach',
                 '你春田必井', '滚！']
-    await session.send(random.choice(zuanyulu), at_sender=True)
+    await bot.send(ev, random.choice(zuanyulu), at_sender=True)
 
 
 @sv.on_command('合刀', aliases=('怎么合刀', '合刀图', '合刀方法'))
-async def hedao(session):
-    await session.send(R.img('合刀.jpg').cqcode)
+async def hedao(bot, ev):
+    await bot.send(ev, R.img('合刀.jpg').cqcode)
 
+@sv.on_fullmatch('mua', only_to_me=True)
+async def chat_mua(bot, ev):
+    await bot.send(ev, '笨蛋~', at_sender=True)
+
+
+@sv.on_fullmatch('来点星奏')
+async def seina(bot, ev):
+    await bot.send(ev, R.img('星奏.png').cqcode)
+
+
+@sv.on_fullmatch(('我有个朋友说他好了', '我朋友说他好了', ))
+async def ddhaole(bot, ev):
+    await bot.send(ev, '那个朋友是不是你弟弟？')
+    # await util.silence(ev, 30)
+
+
+@sv.on_fullmatch('我好了')
+async def nihaole(bot, ev):
+    await bot.send(ev, '不许好，憋回去！')
+    # await util.silence(ev, 30)
 
 # ============================================ #
+
 
 @sv.on_keyword(('确实', '有一说一', 'u1s1', 'yysy'))
 async def chat_queshi(bot, ctx):
@@ -87,9 +83,10 @@ async def chat_queshi(bot, ctx):
         await bot.send(ctx, R.img('确实.jpg').cqcode)
 
 
-@sv.on_keyword(('会战', '刀'))
+
+@sv.on_keyword(('会战'))
 async def chat_clanba(bot, ctx):
-    if random.random() < 0.03:
+    if random.random() < 0.02:
         await bot.send(ctx, R.img('我的天啊你看看都几度了.jpg').cqcode)
 
 
@@ -103,3 +100,14 @@ async def chat_neigui(bot, ctx):
 async def chat_pohai(bot, ctx):
     if random.random() < 0.60:
         await bot.send(ctx, '算我一个！')
+
+nyb_player = f'''{R.img('newyearburst.jpg').cqcode}
+正在播放：New Year Burst
+──●━━━━ 1:05/1:30
+⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
+'''.strip()
+
+@sv.on_keyword(('春黑', '新黑'))
+async def new_year_burst(bot, ev):
+    if random.random() < 0.02:
+        await bot.send(ev, nyb_player)
--- hoshino/modules/groupmaster/group_notice.py
+++ hoshino/modules/groupmaster/group_notice.py
@@ -1,16 +1,17 @@
 from nonebot import on_notice, NoticeSession
 from hoshino import util, R
 
+import hoshino
+from hoshino import Service
+from hoshino.typing import NoticeSession
 
-@on_notice('group_decrease.leave')
-async def leave_notice(session:NoticeSession):
-    cfg = util.load_config(__file__)
-    no_leave_notice_group = cfg.get('no_leave_notice', [])
-    if session.ctx['group_id'] not in no_leave_notice_group:
-        await session.send(f"{session.ctx['user_id']}退群了。")
+sv1 = Service('group-leave-notice')
 
+@sv1.on_notice('group_decrease.leave')
+async def leave_notice(session: NoticeSession):
+    await session.send(f"{session.ctx['user_id']}退群了。")
 
-@on_notice('group_increase')
+@sv2.on_notice('group_increase')
 async def increace_notice(session:NoticeSession):
     pic = R.img(f"欢迎新人.jpg").cqcode
     await session.send(f"\n欢迎加入~"
@@ -18,10 +19,17 @@ async def increace_notice(session:NoticeSession):
                        f"\n是一只AI娘~请多多指教哟！"
                        f"\n{pic}", at_sender=True)
 
+sv2 = Service('group-welcome')
 
-@on_notice('group_decrease.kick_me')
-async def kick_me_alert(session:NoticeSession):
-    group_id = session.event.group_id
-    operator_id = session.event.operator_id
-    coffee = session.bot.config.SUPERUSERS[0]
-    await session.bot.send_private_msg(self_id=session.event.self_id, user_id=coffee, message=f'被Q{operator_id}踢出群{group_id}')
+# @sv2.on_notice('group_increase')
+# async def increace_welcome(session: NoticeSession):
+#
+#     if session.event.user_id == session.event.self_id:
+#         return  # ignore myself
+#
+#     welcomes = hoshino.config.groupmaster.increase_welcome
+#     gid = session.event.group_id
+#     if gid in welcomes:
+#         await session.send(welcomes[gid], at_sender=True)
+#     elif 'default' in welcomes:
+#         await session.send(welcomes['default'], at_sender=True)
