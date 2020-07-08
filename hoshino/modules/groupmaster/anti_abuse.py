d95711eaa2b3937b5c7466799838e03d55268a47
金城刚 <773900035@qq.com>
2020-07-08 17:27:05 +0800

    修改禁言

--- hoshino/modules/groupmaster/anti_abuse.py
+++ hoshino/modules/groupmaster/anti_abuse.py
@@ -65,8 +65,10 @@ async def hb_handler(ctx):
 BANNED_WORD = (
     'rbq', 'RBQ', '憨批', '废物', '死妈', '崽种', '傻逼', '傻逼玩意',
     '没用东西', '傻B', '傻b', 'SB', 'sb', '煞笔', 'cnm', '爬', 'kkp',
-    'nmsl', 'D区', '口区', '我是你爹', 'nmbiss', '弱智', '给爷爬', '杂种爬','爪巴'
+    'nmsl', 'D区', '口区', '我是你爹', 'nmbiss', '弱智', '给爷爬', '杂种爬', '爪巴'
 )
+
+
 @on_command('ban_word', aliases=BANNED_WORD, only_to_me=True)
 async def ban_word(session):
     ctx = session.ctx
@@ -80,4 +82,23 @@ async def ban_word(session):
     hoshino.priv.set_block_user(user_id, timedelta(hours=8))
     pic = R.img(f"chieri{random.randint(1, 4)}.jpg").cqcode
     await session.send(f"不理你啦！バーカー\n{pic}", at_sender=True)
-    await util.silence(session.ctx, 8*60*60)
+    await util.silence(session.ctx, 8 * 60 * 60)
+
+
+CONFESS_WORD = ('我错了', '我认输', '我不该骂你')
+
+
+@on_command('我错了', aliases=CONFESS_WORD, only_to_me=True)
+async def confess_word(session):
+    ctx = session.ctx
+    user_id = ctx['user_id']
+    msg_from = str(user_id)
+    if ctx['message_type'] == 'group':
+        msg_from += f'@[群:{ctx["group_id"]}]'
+    elif ctx['message_type'] == 'discuss':
+        msg_from += f'@[讨论组:{ctx["discuss_id"]}]'
+    hoshino.logger.critical(f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from}: {ctx["message"]}')
+    hoshino.priv.set_block_user(user_id, timedelta(hours=0))
+    pic = R.img(f"chieri{random.randint(1, 4)}.jpg").cqcode
+    await session.send(f"这次就先原谅你啦！バーカー\n{pic}", at_sender=True)
+    await util.silence(session.ctx, 8 * 60 * 60)
